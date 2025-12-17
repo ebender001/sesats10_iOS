d990d51be9f79b9fbfb14ad7eca3886d9054284d
Edward Bender <ebender001@gmail.com>
2025-12-17 11:20:23 -0600

    Initial Commit

---
+++ SESATS10/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SESATS10
+//
+//  Created by Edward Bender on 12/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SESATS10/SESATS10App.swift
@@ -0,0 +1,17 @@
+//
+//  SESATS10App.swift
+//  SESATS10
+//
+//  Created by Edward Bender on 12/17/25.
+//
+
+import SwiftUI
+
+@main
+struct SESATS10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
