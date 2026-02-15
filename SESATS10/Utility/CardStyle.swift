2664662cae46771808d3876bfa501f55999ebd16
Edward Bender <ebender001@gmail.com>
2026-02-15 16:04:12 -0600

    finished theming app

---
+++ SESATS10/Utility/CardStyle.swift
@@ -0,0 +1,8 @@
+//
+//  CardStyle.swift
+//  SESATS10
+//
+//  Created by Edward Bender on 2/15/26.
+//
+
+import Foundation
---
+++ SESATS10/Utility/Theme.swift
@@ -0,0 +1,8 @@
+//
+//  Theme.swift
+//  SESATS10
+//
+//  Created by Edward Bender on 2/15/26.
+//
+
+import Foundation
